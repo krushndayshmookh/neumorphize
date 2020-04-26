c3de9281f1e4ff67b2e19d722f443f341eaf9624
krushndayshmookh <krushndayshmookh@gmail.com>
2020-04-26 13:19:09 +0530

    setup gulp

---
+++ gulpfile.esm.js
@@ -0,0 +1,15 @@
+import { src, dest } from "gulp";
+import sass from "gulp-sass";
+
+sass.compiler = require("node-sass");
+
+export function build_scss() {
+  return src("./sass/**/*.scss")
+    .pipe(sass.sync().on("error", sass.logError))
+    .pipe(dest("./dist/css"));
+}
+
+export default (cb) => {
+  // place code for your default task here
+  cb();
+};
